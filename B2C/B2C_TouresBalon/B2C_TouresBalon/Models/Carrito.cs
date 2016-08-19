3c1cea4c6dd044c149ee658ce21a295e787a4327
Hugo Andrés Mendoza <huancone@gmail.com>
2016-08-19 17:52:50 -0500

    Mas Vistas

---
+++ B2C/B2C_TouresBalon/B2C_TouresBalon/Models/Carrito.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace B2C_TouresBalon.Models
+{
+
+    public class Carrito
+    {
+        List<ItemCarrito> items;
+
+        public bool AgregarProducto(ItemCarrito i)
+        {
+            try
+            {
+                items.Add(i);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ B2C/B2C_TouresBalon/B2C_TouresBalon/Models/ItemCarrito.cs
@@ -0,0 +1,8 @@
+﻿namespace B2C_TouresBalon.Models
+{
+    public class ItemCarrito
+    {
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
