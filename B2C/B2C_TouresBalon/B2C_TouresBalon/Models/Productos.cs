6d80609e2698e99ab620587a0a68047369f98cb7
Hugo Andrés Mendoza <huancone@gmail.com>
2016-08-18 20:15:57 -0500

    MVC lidiando con el consumo del servicio

---
+++ B2C/B2C_TouresBalon/B2C_TouresBalon/Models/Productos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using B2C_TouresBalon.ServicioProductos;
+
+namespace B2C_TouresBalon.Models
+{
+    public class Productos
+    {
+        public List<Producto> promos { get; set; }
+
+        public Productos()
+        {
+        }
+
+        
+
+        public List<Producto> Promociones()
+        {
+            return promos;
+        }
+    }
+}
---
+++ WebServices/Productos/ServicioProductos/ServicioProductos/Clases/TarifaTipo.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ServicioProductos
+{
+    [DataContract]
+    public class TarifaTipo
+    {
+        [DataMember]
+        public int id { get; set; }
+
+        [DataMember]
+        public string nombre_tipo { get; set; }
+
+        [DataMember]
+        public double precio { get; set; }
+
+        public TarifaTipo(int id, string nombreTipo, double precio)
+        {
+            this.id = id;
+            nombre_tipo = nombreTipo;
+            this.precio = precio;
+        }
+    }
+}
