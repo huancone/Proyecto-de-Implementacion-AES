e551fd228ebdb9d3f9dbb86a9963666aa12158d0
haroldchiag <haroldchiag@gmail.com>
2016-09-04 13:03:06 -0500

    Se adiciona la consulta de ordenes, con su detalle y la cancelacion de ordenes

---
+++ B2C/B2CTouresBalon/B2CTouresBalon/Controllers/OrdenController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using B2CTouresBalon.Models;
+using System.ServiceModel;
+
+namespace B2CTouresBalon.Controllers
+{
+    public class OrdenController : Controller
+    {
+        List<Models.OrdenModel> lstordenes = new List<OrdenModel>();
+        // GET: Orden
+        public ActionResult Index()
+        {
+            
+
+            //for (int i=0;i<=10; i++)
+            //{
+            //    OrdenModel ord = new OrdenModel();
+            //    ord.estatus = SrvProxyB2C.EstatusOrden.VALIDACION;
+            //    ord.fechaOrden = DateTime.Now.AddDays(i);
+            //    Random r = new Random(i);
+            //    ord.id_orden = "ORD-" + (Math.Round(r.NextDouble()*10000,0)).ToString() ;
+
+            //    ord.detalle = new List<SrvProxyB2C.Item>();
+
+            //    double valsum = 0; ;
+            //    for (int j = 0; j <= 3; j++)
+            //    {
+            //        SrvProxyB2C.Item item = new SrvProxyB2C.Item();
+            //        item.id_item = "ITM-" + j.ToString();
+            //        item.id_prod = j;
+            //        item.nombre_prod = "Prod-" + j.ToString();
+            //        item.part_num = "Part-" + j.ToString();
+            //        item.cantidad = 1;
+                   
+            //        item.precio = Math.Round( r.NextDouble()*100000,0);
+            //        valsum = valsum + item.precio;
+            //        ord.detalle.Add(item);
+
+            //    }
+            //    ord.precio = valsum;
+            //    lstordenes.Add(ord);
+            //}
+
+
+
+
+            HttpContext.Session["ListaOrden"] = lstordenes;
+
+
+            return View(lstordenes);
+        }
+
+
+        public ActionResult  Cancelar(string idOrden)
+        {
+            ServiceProxyB2C.ServiceProxyB2CClient cancela = new ServiceProxyB2C.ServiceProxyB2CClient();
+            ServiceProxyB2C.RespuestaGenerica rpta = new ServiceProxyB2C.RespuestaGenerica();
+            RespuestaCancelacionModel valorcancela = new Models.RespuestaCancelacionModel();
+            string[] IdOrdenes = new string[1] { idOrden } ;
+
+            try
+            {
+                rpta = cancela.CancelarOrdenes(IdOrdenes);
+            }
+            catch ( FaultException<ServiceProxyB2C.CancelarOrdenesFault> exf )
+            {
+                valorcancela.respuesta = "Se presento un error al cancelar la orden " + idOrden + ". Intentelo más tarde (" + exf.Message + ")"   ;
+                return View("_Cancelar", valorcancela);
+            }
+            catch (Exception  ex)
+            {
+                valorcancela.respuesta = "Se presento un error al cancelar la orden " + idOrden + ". Intentelo más tarde";
+                return View("_Cancelar", valorcancela);
+            }
+            if (rpta == ServiceProxyB2C.RespuestaGenerica.OK)
+                valorcancela.respuesta  = "Se cancelo la orden " + idOrden + " de forma exitosa";
+            else
+                valorcancela.respuesta  = "Se presento un problema al cancelar la orden " + idOrden + ". Intentelo más tarde";
+
+            return View("_Cancelar", valorcancela);
+        }
+
+
+        public ActionResult Detalle (string IdOrden )
+        {
+            lstordenes =(List<OrdenModel>)HttpContext.Session["ListaOrden"] ;
+
+
+            List<ServiceProxyB2C.Item> LstItems = lstordenes.Find(ord => ord.id_orden == IdOrden).detalle;  
+            ViewBag.IdOrden = IdOrden;
+         
+
+            return View("_Detalle", LstItems); 
+        }
+    }
+}
---
+++ B2C/B2CTouresBalon/B2CTouresBalon/Models/OrdenModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using B2CTouresBalon.ServiceProxyB2C;
+
+namespace B2CTouresBalon.Models
+{
+    public class OrdenModel
+    {
+
+
+
+        public DateTime fechaOrden { get; set; } 
+
+        public ServiceProxyB2C.EstatusOrden estatus { get; set; } 
+
+        public string id_orden { get; set; }
+
+        public double precio { get; set; }
+
+        public List<ServiceProxyB2C.Item> detalle { get; set; }
+
+
+    }
+
+    public class RespuestaCancelacionModel
+    {
+        public string respuesta { get; set; }
+    }
+
+    
+}
