96b804aacb40ad06026f6008768eeb5552724904
Hugo Andrés Mendoza <huancone@gmail.com>
2016-09-29 22:42:58 -0500

    v0.7 Terminada la administracion de Usuarios

--- B2C/B2CTouresBalon/B2CTouresBalon/Controllers/CustomerController.cs
+++ B2C/B2CTouresBalon/B2CTouresBalon/Controllers/CustomerController.cs
@@ -18,16 +18,12 @@ namespace B2CTouresBalon.Controllers
         // GET: Customer
         public async Task<ActionResult> Index()
         {
-            return View(await db.CUSTOMER.ToListAsync());
+            return RedirectToAction("Index", "Home");
         }
 
         // POST: Customer/Details/5
         public async Task<ActionResult> Details(decimal id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
             var cUSTOMER = await db.CUSTOMER.FindAsync(id);
             if (cUSTOMER == null)
             {
@@ -49,14 +45,10 @@ namespace B2CTouresBalon.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "CUSTID,FNAME,LNAME,PHONENUMBER,EMAIL,PASSWORD,CREDITCARDTYPE,CREDITCARDNUMBER,STATUS")] CUSTOMER cUSTOMER)
         {
-            if (ModelState.IsValid)
-            {
-                db.CUSTOMER.Add(cUSTOMER);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-
-            return View(cUSTOMER);
+            if (!ModelState.IsValid) return View(cUSTOMER);
+            db.CUSTOMER.Add(cUSTOMER);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index","Home");
         }
 
         // GET: Customer/Edit/5
@@ -79,15 +71,12 @@ namespace B2CTouresBalon.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "CUSTID,FNAME,LNAME,PHONENUMBER,EMAIL,PASSWORD,CREDITCARDTYPE,CREDITCARDNUMBER,STATUS")] CUSTOMER cUSTOMER)
+        public async Task<ActionResult> Edit([Bind(Include = "FNAME,LNAME,PHONENUMBER,EMAIL,PASSWORD,CREDITCARDTYPE,CREDITCARDNUMBER")] CUSTOMER cUSTOMER)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(cUSTOMER).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(cUSTOMER);
+            if (!ModelState.IsValid) return View(cUSTOMER);
+            db.Entry(cUSTOMER).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
        protected override void Dispose(bool disposing)
